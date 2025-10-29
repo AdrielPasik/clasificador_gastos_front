be8f3099075966dbc936b432d88e48aa0bf2d967
Maximo Bianchi Canelo <63102600+MassiB09@users.noreply.github.com>
2025-10-29 10:59:30 -0300

    Update next.config.mjs for S3 deployment
    Removed duplicate images configuration and added output setting for S3.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <--- genera la carpeta /out para S3
+  images: {
+    unoptimized: true, // <--- necesario porque no hay Image Optimization en S3
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
